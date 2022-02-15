9a29b21d9e6b962e73e46c4d01a755648b2f2ce5
dyushakolesov <dyushakolesov@mail.ru>
2022-02-15 14:39:43 +0300

    ticker normalization fixed

--- ticker_normalization.py
+++ ticker_normalization.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import re
-country_names = {' USA',' Australia',' China',' Spain'} #worth it or not?
-stop_words = {' Class', ' Series', ' Depositary', ' Common', ' Ordinary Share',' Common Stock',' Warrant', ' Warrants',' Units', ' Unit',
-                ' Co.',' Company',' Corp',' Inc.',' Ltd',' Incorporated', ' Group',' Holdings'} #TEMP
+#country_names = {' USA',' Australia',' China',' Spain'} #worth it or not?
+stop_words = {' Class', ' Series', ' Depositary', ' Common', ' Ordinary Share',' Common Stock',' Warrant', ' Warrants',' Units', ' Unit',' Co ',' Company',' Corp',' Inc',' Ltd',' Incorporated',' SA '," Plc"," Sab "," NV"} #TEMP
+special_words = {' Group',' Holdings'}
 
 def ticker_is_primary(str):
     if (str.find(' due') == -1 and str.find(' Due') == -1 and len(str) < 80 and str.find(' Warrant') == -1 and str.find(' warrant') == -1 and str.find(' Right') == -1 and str.find('%') == -1):
@@ -18,18 +18,28 @@ def string_normalize(str):
 
     #step 1: legal ang special charachter removal:
     str = ''.join(re. sub(r'\([^)]*\)', '', str))
+    for char in special_chars:
+        str = str.replace(char,'')
     #step 2: case normalization
     str = str.title()
+
+    #step 3: country name removal
+    #for name in country_names:
+    #    str = str.replace(name,'')
+    str = str.replace('Corporation','Corp')
+    str = str.replace(' Of ',' ')
     #step xx: remove words that useless for matching
     for word in stop_words:
         str = str.partition(word)[0]
-    #step 3: country name removal
-    for name in country_names:
-        str = str.replace(name,'')
-    str = str.replace('Corporation','Corp')
-    str = str.replace(' Of ',' ')
-    for char in special_chars:
-        str = str.replace(char,'')
+    for word in special_words:
+        str = str.partition(word)[0] + str.partition(word)[1]
+    
+    if(str.find("Co") == len(str) - len("Co")):
+        str = str[:-len("Co")]
+    if(len(str) > 2 and str.find("And") == len(str) - len("And")):
+        str = str[:-len("And")]
+    str = str.replace("Hp","Hewlett-Packard")
+    str = str.replace('  ',' ')
     if(str[-1]==' '):
         str = str[0:len(str)-1]
     return str
@@ -50,4 +60,4 @@ for index,row in df_tickers.iterrows():
 df_tickers = df_tickers[df_tickers.index.isin(ticker_indicies)]
 
     
-df_tickers.to_csv("normalized tickers holdings.csv",index=False)
+df_tickers.to_csv("fix normalized tickers holdings.csv",index=False)
